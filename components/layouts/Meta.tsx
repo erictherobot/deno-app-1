cb1415be6da416884e459f5480945c7230ec265f
Eric David Smith <erictherobot@gmail.com>
2022-07-19 13:26:22 -0400

    Adding SEO tags

--- components/layouts/Meta.tsx
+++ components/layouts/Meta.tsx
@@ -13,6 +13,11 @@ export const Meta = ({
   <head>
     <title>{title}</title>
     <meta name="description" content={description} />
+    <meta
+      name="keywords"
+      content="Deno Fresh Web Framework, TypeScript, HTML, CSS, Javascript, Deno"
+    />
+    <meta name="author" content="Eric David Smith <erictherobot@gmail.com>" />
     <link
       rel="canonical"
       href={`https://erictherobot-deno-app-1.deno.dev${canonical}`}
